53b40c5d325cd1160dfb81a7a035821cd4c8e9c8
Konstantin V. Krupovich <kkrupovich@rossul.com>
2023-04-30 23:51:57 +0300

    #88 adjusting accuracy/dodge during attack trial

--- Assets/Scripts/ECS/Systems/Battle/BattleAttackSystem.cs
+++ Assets/Scripts/ECS/Systems/Battle/BattleAttackSystem.cs
@@ -9,14 +9,15 @@ namespace Assets.Scripts.ECS.Systems
 {
     public class BattleAttackSystem : IEcsRunSystem
     {
+        private readonly EcsWorldInject ecsWorld = default;
+        
         private readonly EcsPoolInject<BattleTurnInfo> turnInfoPool = default;
         private readonly EcsPoolInject<DealDamageTag> dealDamageTagPool = default;
         private readonly EcsPoolInject<DealEffectsTag> dealEffectsTagPool = default;
+        private readonly EcsPoolInject<RelationEffectsComp> relEffectsPool = default;
 
         private readonly EcsPoolInject<AttackerRef> attackerRefPool = default;
         private readonly EcsPoolInject<TargetRef> targetRefPool = default;
-        private readonly EcsPoolInject<IntValueComp<AccuracyRateTag>> accuracyPool = default;
-        private readonly EcsPoolInject<IntValueComp<DodgeRateTag>> dodgePool = default;
 
         private readonly EcsFilterInject<
             Inc<BattleTurnInfo, MakeTurnTag, AttackTag, AttackerRef, TargetRef>> filter = default;
@@ -34,20 +35,20 @@ namespace Assets.Scripts.ECS.Systems
             ref var attackerRef = ref attackerRefPool.Value.Get(turnEntity);
             ref var targetRef = ref targetRefPool.Value.Get(turnEntity);
 
-            if (!attackerRef.Packed.Unpack(out _, out var attakerEntity))
+            if (!attackerRef.Packed.Unpack(out _, out var attackerEntity))
                 throw new Exception("No attacker");
 
             if (!targetRef.Packed.Unpack(out _, out var targetEntity))
                 throw new Exception("No target");
 
-            ref var accuracyComp = ref accuracyPool.Value.Get(attakerEntity);
-            ref var dodgeComp = ref dodgePool.Value.Get(targetEntity);
+            var accuracyRate = GetAdjustedValue<AccuracyRateTag>(attackerEntity, SpecOption.AccuracyRate);
+            var dodgeRate = GetAdjustedValue<DodgeRateTag>(attackerEntity, SpecOption.DodgeRate);
 
             // attack:
-            var accurate = prefs.Value.DisableRNGToggle || accuracyComp.Value.RatedRandomBool();
+            var accurate = prefs.Value.DisableRNGToggle || accuracyRate.RatedRandomBool();
 
             // defence:
-            var dodged = !prefs.Value.DisableRNGToggle && dodgeComp.Value.RatedRandomBool();
+            var dodged = !prefs.Value.DisableRNGToggle && dodgeRate.RatedRandomBool();
 
             if (accurate && !dodged)
             {
@@ -59,6 +60,49 @@ namespace Assets.Scripts.ECS.Systems
             turnInfo.Dodged = dodged;
             turnInfo.State = TurnState.TurnInProgress;
         }
+
+        private int GetAdjustedValue<T>(int entity, SpecOption specOption)
+            where T : struct
+        {
+            var raw = ecsWorld.Value.ReadIntValue<T>(entity);
+            var adjustmentFactor = GetAdjustmentValue(entity, specOption);
+            int retval = (int)(raw * adjustmentFactor);
+            return retval;
+        }
+
+        private float GetAdjustmentValue(int entity, SpecOption specOption)
+        {
+            var retval = 1f; 
+            ref var relationEffects = ref relEffectsPool.Value.Get(entity);
+            foreach (var relEffect in relationEffects.CurrentEffects)
+            {
+                if (relEffect.Key.SpecOption != specOption)
+                    continue;
+
+                switch (relEffect.Value.Rule.EffectType)
+                {
+                    case RelationsEffectType.SpecMaxMin:
+                        break;
+                    case RelationsEffectType.SpecAbs:
+                        {
+                            var rule = (EffectRuleSpecAbs)relEffect.Value.Rule;
+                            retval -= rule.Value / 100f;
+                        }
+                        break;
+                    case RelationsEffectType.SpecPercent:
+                        {
+                            var rule = (EffectRuleSpecAbs)relEffect.Value.Rule;
+                            retval *= rule.Value / 100f;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return retval;
+        }
+
 
     }
 }
